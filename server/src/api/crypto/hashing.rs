90c11c2d2b3dfc46467b89f3383053132258457b
Michal Janočko <33790048+michaljanocko@users.noreply.github.com>
2023-01-17 11:04:25 +0100

    🐛 Fix a borrow checker bug in hashing

--- server/src/api/crypto/hashing.rs
+++ server/src/api/crypto/hashing.rs
@@ -1,19 +1,16 @@
 use anyhow::anyhow;
-use argon2::{password_hash::SaltString, Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
-use rsa::rand_core::OsRng;
+use argon2::{password_hash::Salt, Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
 
 pub fn parse_hash(hash: &str) -> anyhow::Result<PasswordHash> {
     PasswordHash::new(hash).map_err(|_| anyhow!("Failed to parse hash"))
 }
 
-pub fn hash(secret: &[u8]) -> anyhow::Result<(PasswordHash, SaltString)> {
-    let salt = SaltString::generate(OsRng);
-
+pub fn hash<'a>(salt: &'a Salt, secret: &[u8]) -> anyhow::Result<PasswordHash<'a>> {
     let secret_hash = Argon2::default()
-        .hash_password(secret, &salt)
+        .hash_password(secret, salt)
         .map_err(|_| anyhow!("Failed to hash secret"))?;
 
-    Ok((secret_hash, salt))
+    Ok(secret_hash)
 }
 
 pub fn verify(secret: &[u8], hash: &PasswordHash) -> bool {
