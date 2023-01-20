a28a9be98bbf8f521f74097adf2e76a96e4a3d8e
Michal Janočko <33790048+michaljanocko@users.noreply.github.com>
2023-01-20 18:41:25 +0100

    🚧 Trying to render an SPA

---
+++ browser/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { browser } from '$app/environment'
+
+  if (
+    browser &&
+    document.cookie
+      .split(';')
+      .some((cookie) => cookie.trim().startsWith('_patrol_key='))
+  )
+    location.replace('/')
+</script>
+
+<slot />
--- browser/src/routes/+page.svelte
+++ browser/src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts">
+  export const prerender = false
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
+   to read the documentation
+</p>
